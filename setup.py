3137afb1be6d430b06af4756984d47957ba464ba
Erik M. Bray <embray@stsci.edu>
2015-07-15 17:56:24 -0400

    The upgraded ez_setup.py however does not work with Python 2.5 (nor has setuptools itself for a long time (not since version 2.0)); although I will not drop 2.5 support for d2to1 itself, the rare 2.5 user is on their own wrt setuptools I think

--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@
 try:
     from setuptools import setup
 except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+    try:
+        from ez_setup import use_setuptools
+        use_setuptools()
+        from setuptools import setup
+    except SyntaxError:
+        raise ImportError(
+            "d2to1 and projects that use it require setuptools to be "
+            "installed; automatic bootstrapping of setuptools is not "
+            "supported on Python 2.5")
 
 # d2to1 basically installs itself!  See setup.cfg for the project metadata.
 from d2to1.util import cfg_to_args
